a96ed6d74acd7fc5d2e5e38836f4cbb2f9a53bad
pradministrator <prashanthhn2509@gmail.com>
2018-07-27 16:52:06 +0530

    CommandExecuterAPI_Plan

---
+++ CommandExecuterAPI.js
@@ -0,0 +1,12 @@
+/*
+
+To do : 
+    Implement get methods to execute commands
+        The URL format :
+            /Execute/command=<command>
+        Response :
+            JSON => {ERROR : <string>,
+                    STDOUT : <string>,
+                    STDERR : <string>}
+
+*/
